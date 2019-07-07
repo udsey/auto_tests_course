6a397ec54acb996d90e1bb46844a3a9cc323818f
udsey <yuuhik@yandex.ru>
2019-07-07 11:04:42 +0300

    change for 3.3 step

--- test_form.py
+++ test_form.py
@@ -1,47 +1,40 @@
-
+import unittest
 from selenium import webdriver
 import time
 
-link2 = "http://suninjuly.github.io/registration2.html"
-link1 = "http://suninjuly.github.io/registration1.html"
 
-def test_registration_form_test(link):
+
+def registration_check(link):
 
     browser = webdriver.Chrome()
-
     browser.get(link)
 
     input_name = browser.find_element_by_xpath("//           div[@class='first_block']//input[@class='form-control first']")
-    input_family_name  = browser.find_element_by_xpath("//div[@class='first_block']//input[@class='form-control second']")
-    input_email = browser.find_element_by_xpath("//div[@class='first_block']//input[@class='form-control third']")
-
-
     input_name.send_keys("Ivan")
+    input_family_name  = browser.find_element_by_xpath("//div[@class='first_block']//input[@class='form-control second']")
     input_family_name.send_keys("Ivanov")
+    input_email = browser.find_element_by_xpath("//div[@class='first_block']//input[@class='form-control third']")
     input_email.send_keys("ivan@ivanov.ru")
 
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
     time.sleep(1)
-
     welcome_text_elt = browser.find_element_by_tag_name("h1")
     welcome_text = welcome_text_elt.text
     return welcome_text
 
-"Поздравляем! Вы успешно зарегистировались!", "Oops"
 
 
-import unittest
 
 class TestLinks(unittest.TestCase):
     def test_link1(self):
-        self.assertEqual(test_registration_form_test(link1), "Поздравляем! Вы успешно зарегистировались!" , "oops")
+        text_a=registration_check("http://suninjuly.github.io/registration1.html")
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", text_a)
 
     def test_link2(self):
-        self.assertEqual(test_registration_form_test(link2), "Поздравляем! Вы успешно зарегистировались!" , "oops")
-
-
+        text_a=registration_check("http://suninjuly.github.io/registration2.html")
+        self.assertEqual("Поздравляем! Вы успешно зарегистировались!", text_a )
 
 if __name__=="__main__":
     unittest.main()
